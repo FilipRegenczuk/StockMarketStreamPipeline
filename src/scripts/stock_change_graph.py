0bb9de2d4ba8fafbb02ff98aef62594ac397f582
Filip-Regenczuk <filip_regenczuk@mckinsey.com>
2023-09-04 16:59:36 +0200

    first version of graph

--- src/scripts/stock_change_graph.py
+++ src/scripts/stock_change_graph.py
@@ -1,8 +1,43 @@
+import numpy as np
+import collections
+import matplotlib.pyplot as plt
 from opensearchpy import OpenSearch
+from matplotlib.animation import FuncAnimation
 
 class StockChangeGraph:
     def __init__(self):
-        pass
+        self.os_client = OpenSearchClient(
+            host='localhost',
+            port=9200
+        )
+    
+    def create_animation_plot(self):
+        value = collections.deque(np.zeros(10))
+        fig = plt.figure(figsize=(12,6), facecolor='#DEDEDE')
+        ax = plt.subplot(121)
+        ax.set_facecolor('#DEDEDE')
+
+        def update_action_value(i):
+            # get data
+            value.popleft()
+            curr_value = self.os_client.get_current_value('stock')
+            value.append(curr_value)
+
+            # clear axis
+            ax.cla()
+
+            # plot action value
+            ax.plot(value)
+            ax.scatter(len(value) - 1, value[-1])
+            ax.text(len(value) - 1, value[-1] + 2, value[-1])
+            # ax.set_ylim
+
+        # animate plot
+        ani = FuncAnimation(fig, update_action_value, interval=1000)
+        plt.show()
+
+    def run(self):
+        self.create_animation_plot()
 
 
 class OpenSearchClient:
@@ -30,12 +65,16 @@ class OpenSearchClient:
     
     def get_current_value(self, index: str):
         query = """{"sort": [{"Date": {"order": "desc"}}], "size": 1}"""
+        result = self.client.search(index='stock', body=query)
         curr_value = (
-            self.get_documents(index, query)
+            result
             .get('hits').get('hits')[0].get('_source').get('Close')
         )
         return curr_value
 
 
-client = OpenSearchClient('localhost', 9200)
+# client = OpenSearchClient('localhost', 9200)
 # print(client.info)
+
+graph = StockChangeGraph()
+graph.run()
