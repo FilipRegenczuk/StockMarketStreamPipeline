a7369db55d68ccf02977f55f5ca7f84594cd36cc
Filip-Regenczuk <filip_regenczuk@mckinsey.com>
2023-09-04 23:02:53 +0200

    some fixes in graph

--- src/scripts/stock_change_graph.py
+++ src/scripts/stock_change_graph.py
@@ -5,6 +5,9 @@ from opensearchpy import OpenSearch
 from matplotlib.animation import FuncAnimation
 
 class StockChangeGraph:
+    """
+    TODO
+    """
     def __init__(self):
         self.os_client = OpenSearchClient(
             host='localhost',
@@ -13,24 +16,28 @@ class StockChangeGraph:
     
     def create_animation_plot(self):
         value = collections.deque(np.zeros(10))
+        date = collections.deque(np.zeros(10))
         fig = plt.figure(figsize=(12,6), facecolor='#DEDEDE')
-        ax = plt.subplot(121)
-        ax.set_facecolor('#DEDEDE')
+        ax = plt.subplot(1,1,1)
 
         def update_action_value(i):
             # get data
+            date.popleft()
             value.popleft()
-            curr_value = self.os_client.get_current_value('stock')
+            curr_date, curr_value = self.os_client.get_current_value('stock')
+            date.append(curr_date)
             value.append(curr_value)
 
             # clear axis
             ax.cla()
 
             # plot action value
-            ax.plot(value)
-            ax.scatter(len(value) - 1, value[-1])
-            ax.text(len(value) - 1, value[-1] + 2, value[-1])
-            # ax.set_ylim
+            ax.ticklabel_format(useOffset=False, style='plain')
+            ax.plot(date, value)
+            ax.scatter(date[-1], value[-1])
+            ax.text(date[-1], value[-1] + 1, f"{value[-1]:.{2}f}$")
+            ax.set_ylim(0, 300)
+            ax.set_xlim(curr_date - 10, curr_date)
 
         # animate plot
         ani = FuncAnimation(fig, update_action_value, interval=1000)
@@ -65,16 +72,12 @@ class OpenSearchClient:
     
     def get_current_value(self, index: str):
         query = """{"sort": [{"Date": {"order": "desc"}}], "size": 1}"""
-        result = self.client.search(index='stock', body=query)
-        curr_value = (
-            result
-            .get('hits').get('hits')[0].get('_source').get('Close')
-        )
-        return curr_value
+        result = self.client.search(index=index, body=query)
+        result_fields = result.get('hits').get('hits')[0].get('_source')
+        curr_value = result_fields.get('Close')
+        curr_date = result_fields.get('Date')
+        return (curr_date, curr_value)
 
 
-# client = OpenSearchClient('localhost', 9200)
-# print(client.info)
-
-graph = StockChangeGraph()
+graph = StockChangeGraph('')
 graph.run()
